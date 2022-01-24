b3346ff9e6c74ffd43e37d8022788b57f8e72fa5
Dalton <slattery.da@gmail.com>
2022-01-24 16:32:29 -0600

    html additions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-// make a db.json server with a limit amount of file
-const apiKey = ''
-const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
-fetch(url)
-    .then(r=>r.json())
-    .then(console.log)
+// // make a db.json server with a limit amount of file
+
+
+
+// const url = `https://api.nasa.gov/planetary/apod?api_key=${daltonAPI}`
+// fetch(url)
+//     .then(r => r.json())
+//     .then(console.log)
