55618ae441849a15b1db8034742f8349b2bb2dd5
Dalton <slattery.da@gmail.com>
2022-01-24 16:08:25 -0600

    starting fetch

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// make a db.json server with a limit amount of files
+// make a db.json server with a limit amount of file
+const apiKey = ''
+const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+fetch(url)
+    .then(r=>r.json())
+    .then(console.log)
