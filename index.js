da537ed318397d8407573c379a50e0cccfe21672
Dalton <slattery.da@gmail.com>
2022-01-25 10:28:59 -0600

    building functions

--- index.js
+++ index.js
@@ -6,50 +6,55 @@ subForm.addEventListener('submit', e => {
     e.preventDefault()
     const apiKey = document.getElementById('apiKey').value
 //fetch 10 photos from NASA API
-    fetch(url+`${apiKey}`+'&count=50', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-        
-    })
+    fetch(url+`${apiKey}`+'&count=1' //{
+    //     method: 'GET',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
+    // }
+    )
     .then(r => r.json())
-    .then(console.log)
+    .then(arr => arr.forEach(renderPhoto))
 })
 
 function renderPhoto(obj){
-    const newImg = document.createElement('img')
-    const imageContainer = document.getElementById('image-section')
-    newImg.src = obj.hdurl
-    newImg.alt = obj.title
-    imageContainer.appendChild(newImg)
+    if (obj.media_type = 'image'){
+        console.log(obj)
+        const newImg = document.createElement('img')
+        const gallery = document.getElementById('gallery-section')
+        //check for hdurl or url
+        newImg.src = obj.hdurl
+        newImg.alt = obj.title
+        newImg.addEventListener('click', e => {
+            console.log(e.target)
+            showPhoto(obj)
+
+        })
+        gallery.appendChild(newImg)
+    }
+}
+
+function showPhoto(obj){
+    //info needed:
+    //explanation, date, hdurl, title, 
+    const photoDate = obj.date
+    const photoExplanation = obj.explanation
+    const photoHDUrl = obj.hdurl
+    const photoTitle = obj.title
+    const imageCard = document.getElementById('image-section')
+    console.log(obj)    
+    
 }
 
 
-// function difference(date1, date2) {  
-//     const date1utc = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
-//     const date2utc = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
-//       day = 1000*60*60*24;
-//     return(date2utc - date1utc)/day
-//   }
-  
-//   const date1 = new Date("2020-12-10"),
-//         date2 = new Date("2021-10-31"),
-//         time_difference = difference(date1,date2)
-  
-//   console.log(time_difference)
-
-
-    
 //Important
 //Img
 
-
-//{copyright: 'Matipon TangmatithamNARIT Text: Matipon Tangmatitham', date: '2022-01-25', explanation: 'Which direction is this comet heading?  Judging by…images submitted to APOD of Comet Leonard in 2021', media_type: 'video', service_version: 'v1', …}
-// copyright: "Matipon TangmatithamNARIT Text: Matipon Tangmatitham"
-// date: "2022-01-25"
-// explanation: "Which direction is this comet heading?  Judging by the tail, one might imagine that Comet Leonard is traveling towards the bottom right, but a full 3D analysis shows it traveling almost directly away from the camera.  With this perspective, the dust tail is trailed towards the camera and can only be seen as a short yellow-white glow near the head of the comet.  The bluish ion tail, however, is made up of escaping ions that are forced directly away from the Sun by the solar wind -- but channeled along the Sun's magnetic field lines.  The Sun's magnetic field is quite complex, however, and occasionally solar magnetic reconnection will break the ion tail into knots that are pushed away from the Sun. One such knot is visible in the featured one-hour time-lapse video captured in late December from Thailand.  Comet Leonard is now fading as it heads out of our Solar System.    Gallery:  Notable images submitted to APOD of Comet Leonard in 2021"
-// media_type: "video"
+// {date: '1995-07-08', explanation: 'July 8, 1995   Damage to Apollo 13  Picture Credit…ghted to Robert J. Nemiroff and Jerry T. Bonnell.', hdurl: 'https://apod.nasa.gov/apod/image/a13_servicemod.gif', media_type: 'image', service_version: 'v1', …}
+// date: "1995-07-08"
+// explanation: "July 8, 1995   Damage to Apollo 13  Picture Credit: NASA, Crew of Apollo 13 Explanation:  In April of 1970, after an oxygen tank exploded and damaged their service module, the Apollo 13 astronauts were forced to abandon their plans to make the third manned lunar landing. The extent of the damage is revealed in this photo, taken as the crippled module was drifting away - jettisoned prior to their reentry and eventual safe splashdown. An entire panel on the right side of the module is seen to have been blown away and damage to internal structures is apparent.  For more information about the picture see the NASA photo caption.  We keep an archive of Astronomy Pictures of the Day.   Astronomy Picture of the Day is brought to you by  Robert Nemiroff and  Jerry Bonnell . Original material on this page is copyrighted to Robert J. Nemiroff and Jerry T. Bonnell."
+// hdurl: "https://apod.nasa.gov/apod/image/a13_servicemod.gif"
+// media_type: "image"
 // service_version: "v1"
-// title: "Video: Comet Leonard over One Hour"
-// url: "https://www.youtube.com/embed/s6IpsM_HNcU?rel=0"
+// title: "Damage to Apollo 13"
+// url: "https://apod.nasa.gov/apod/image/a13_servicemod.gif"
