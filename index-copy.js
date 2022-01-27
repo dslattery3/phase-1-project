db141989dff4d2e466812398d0ab6b26efb838c5
Dalton <slattery.da@gmail.com>
2022-01-27 17:59:59 -0600

    NASA API down. Have db.json server to run off. Added new features.

---
+++ index-copy.js
@@ -0,0 +1,141 @@
+// //ORIGINAL WORK WHEN NASA API IS BACK UP
+
+
+// const url = `https://api.nasa.gov/planetary/apod?api_key=`
+// const subForm = document.getElementById('form-submit-api')
+// let saveCounter = 0
+// const newDay = new Date()
+// const today = newDay.getFullYear()+'-'+(newDay.getMonth()+1)+'-'+newDay.getDate();
+// fetch(`https://api.nasa.gov/planetary/apod?api_key=${api}&start_date=${today}&end_date=${today}`)
+//     .then(r => r.json())
+//     .then(data => {
+//         console.log(data)
+//         showPhoto(data[0])
+//         renderPhoto(data[0])
+//     })
+// subForm.addEventListener('submit', e => {
+//     e.preventDefault()
+//     const apiKey = document.getElementById('apiKey').value
+//     if(apiKey == ''){
+//         apiKey = api
+//     }
+//     fetch(url + `${apiKey}` + '&count=5')
+//         .then(r => r.json())
+//         .then(arr => {
+//             showPhoto(arr[0])
+//             arr.forEach(renderPhoto)
+//         })
+//     loading()
+// })
+// const dateForm = document.getElementById('form-pick-date')
+// dateForm.addEventListener('submit', e => {
+//     e.preventDefault()
+//     const requestDate = document.getElementById('end-date').value
+//     console.log(requestDate)
+//     if(apiKey == ''){
+//         apiKey = api
+//     }
+//     fetch(`https://api.nasa.gov/planetary/apod?api_key=${api}&start_date=${requestDate}&end_date=${requestDate}`)
+//         .then(r=>r.json())
+//         .then(data => {
+//             renderPhoto(data[0])
+//             showPhoto(data[0])
+//         })
+//         dateForm.reset()
+//         playSpace()
+// })
+// function renderPhoto(obj) {
+//     if (obj.media_type = 'image') {
+//         const newImg = document.createElement('img')
+//         const gallery = document.getElementById('gallery-section')
+//         newImg.src = obj.url
+//         newImg.alt = obj.title
+//         newImg.addEventListener('mouseover', e =>{
+//             e.target.style.width = '125px'
+//             e.target.style.height = '125px'
+//         })
+//         newImg.addEventListener('mouseout', e => {
+//             e.target.style.width ='90px'
+//             e.target.style.height = '90px'
+//         })
+//         newImg.addEventListener('click', () => {
+//             showPhoto(obj)
+//         })
+//         gallery.appendChild(newImg)
+//     }
+// }
+// function showPhoto(obj) {
+//     const photoDate = document.getElementById('photo-date');
+//     photoDate.innerText = dateFormatting(obj.date);
+
+//     const photoExplanation = document.querySelector('p');
+//     photoExplanation.textContent = obj.explanation;
+
+//     const photoUrl = document.querySelector('#image-section img');
+//     photoUrl.src = obj.url;
+//     photoUrl.alt = obj.title;
+
+//     const photoTitle = document.querySelector('.title');
+//     photoTitle.textContent = obj.title;
+// }
+// function dateFormatting(oldDate) {
+//     if(oldDate[2] == '/'){
+//         return oldDate
+//     }
+//     else{
+//        const formattedDate = oldDate.split("-");
+//     return `${formattedDate[1]}/${formattedDate[2]}/${formattedDate[0]}` 
+//     }
+// }
+// const saveButton = document.querySelector('.save-button')
+// saveButton.addEventListener('click', e => {
+//     let passObj = {
+//         title: "",
+//         date: "",
+//         url: "",
+//         explanation: ""
+//     }
+//     passObj.title = document.querySelector('.title').textContent;
+//     passObj.date = document.querySelector('#photo-date').textContent;
+//     passObj.url = document.querySelector('#image-section img').src;
+//     passObj.explanation = document.querySelector('#image-section p').innerText;
+//     savePhoto(passObj);
+// })
+// let arrSaved = [];
+// function savePhoto(incomeObj) {
+//     if (arrSaved.find( ({title}) => title === incomeObj.title)){
+//         alert('Photo already liked')
+//         return
+//     }
+//     arrSaved[saveCounter] = incomeObj
+//     let placeForSaved = document.querySelector('#liked-photos');
+//     let imgItem = document.createElement("img");
+//     imgItem.src = arrSaved[saveCounter].url
+//     imgItem.alt = arrSaved[saveCounter].title
+//     imgItem.addEventListener('mouseover', e =>{
+//         e.target.style.width = '125px'
+//         e.target.style.height = '125px'
+//     })
+//     imgItem.addEventListener('mouseout', e => {
+//         e.target.style.width ='90px'
+//         e.target.style.height = '90px'
+//     })
+//     imgItem.addEventListener('click', () => {
+//         showPhoto(incomeObj)
+//     })
+//     placeForSaved.appendChild(imgItem);
+//     saveCounter++
+//     likeIt()
+// }
+// const spaceSound = new Audio('./spacee.mp3')
+// function playSpace() {
+//     spaceSound.play()
+// }
+// const loadingSound = new Audio('./squid-game.mp3')
+// function loading() {
+//     loadingSound.play()
+// }
+// const loveIt = new Audio('./love-it.mp3')
+// function likeIt() {
+//     loveIt.play()
+// }
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const url = `https://api.nasa.gov/planetary/apod?api_key=`
+// FOR DEMO PURPOSES ONLY
+// NASA API DOWN ON 27JAN2022
+// USE WITH DB.JSON SERVER
+// IF NASA API BACK UP SWITCH TO index-copy.js in index.html
+
+const url = `http://localhost:3000/apods`
 const subForm = document.getElementById('form-submit-api')
 let saveCounter = 0
+let demoCounter = 30
 const newDay = new Date()
 const today = newDay.getFullYear()+'-'+(newDay.getMonth()+1)+'-'+newDay.getDate();
-fetch(`https://api.nasa.gov/planetary/apod?api_key=${api}&start_date=${today}&end_date=${today}`)
+fetch(url)
     .then(r => r.json())
     .then(data => {
         console.log(data)
@@ -12,34 +18,64 @@ fetch(`https://api.nasa.gov/planetary/apod?api_key=${api}&start_date=${today}&en
     })
 subForm.addEventListener('submit', e => {
     e.preventDefault()
-    const apiKey = document.getElementById('apiKey').value
-    if(apiKey == ''){
-        apiKey = api
-    }
-    fetch(url + `${apiKey}` + '&count=5')
+    let apiKey = document.getElementById('apiKey').value
+
+    if(apiKey.length == 0 || apiKey === 'DEMO_KEY'){
+        apiKey = 'DEMO_KEY'
+        demoCounter--
+        alert(`Invalid API Key detected, DEMO_KEY used. You have ${demoCounter} uses left.`)
+        }
+    fetch(url)
         .then(r => r.json())
         .then(arr => {
             showPhoto(arr[0])
             arr.forEach(renderPhoto)
         })
-    //loading()
+    loading()
 })
 const dateForm = document.getElementById('form-pick-date')
 dateForm.addEventListener('submit', e => {
     e.preventDefault()
-    const requestDate = document.getElementById('end-date').value
-    if(apiKey == ''){
-        apiKey = api
+    let requestDate = document.getElementById('end-date').value
+    if(requestDate == ''){
+        alert('Please enter a date')
+        return
     }
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=${api}&start_date=${requestDate}&end_date=${requestDate}`)
+    else if(requestDate[4] != '-'){
+        alert('Please follow the above format')
+        dateForm.reset()
+        return
+    }
+    else if(requestDate[7] != '-'){
+        alert('Please follow the above format')
+        dateForm.reset()
+        return
+    }
+    else if(requestDate.length != 10){
+        alert('Please follow the above format')
+        dateForm.reset()
+        return
+    }
+    // if(apiKey == ''){
+    //     apiKey = api
+    // }
+    fetch(url)
         .then(r=>r.json())
         .then(data => {
-            renderPhoto(data[0])
-            showPhoto(data[0])
+            findDateFakeAPI(data, requestDate)
         })
         dateForm.reset()
-        // playSpace()
+        playSpace()
 })
+function findDateFakeAPI(arr, str){
+    for (let i=0; i < arr.length; i++){
+        if (arr[i].date === str){
+            renderPhoto(arr[i])
+            showPhoto(arr[i])
+        }
+    }
+}
+
 function renderPhoto(obj) {
     if (obj.media_type = 'image') {
         const newImg = document.createElement('img')
@@ -121,17 +157,17 @@ function savePhoto(incomeObj) {
     })
     placeForSaved.appendChild(imgItem);
     saveCounter++
-    // likeIt()
+    likeIt()
 }
-// const spaceSound = new Audio('./spacee.mp3')
-// function playSpace() {
-//     spaceSound.play()
-// }
-// const loadingSound = new Audio('./squid-game.mp3')
-// function loading() {
-//     loadingSound.play()
-// }
-// const loveIt = new Audio('./love-it.mp3')
-// function likeIt() {
-//     loveIt.play()
-// }
+const spaceSound = new Audio('./spacee.mp3')
+function playSpace() {
+    spaceSound.play()
+}
+const loadingSound = new Audio('./squid-game.mp3')
+function loading() {
+    loadingSound.play()
+}
+const loveIt = new Audio('./love-it.mp3')
+function likeIt() {
+    loveIt.play()
+}
